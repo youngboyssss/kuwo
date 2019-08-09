725bea0f32a7ce13dbfadcd7228511c15e660eef
fannie <1292947570@qq.com>
2019-08-09 22:10:20 +0800

    ftt

--- kugou/src/views/Mv/MvDetailPlay/MvDetailPlay.js
+++ kugou/src/views/Mv/MvDetailPlay/MvDetailPlay.js
@@ -21,12 +21,12 @@ class MvDetailPlay extends React.Component{
                     <Link to={"/searchList"}><img className={'mvSearch3'} src={"http://image.kuwo.cn/mpage/html5/2015/tuijian/seach.png"}/></Link>
                 </div>
                 <div className={"contentWrap"}>
-                    <div className={"kongdiv"}></div>
+                    <div className={"mvkongdiv"}></div>
                     <div className={"videoPlay"}>
                         <video className={"videoId"} controls={"controls"} autoPlay={"autoPlay"} src={"http://antiserver.kuwo.cn/anti.s?rid=MUSIC_"+this.props.match.params.id+"&response=res&format=mp4&type=convert_url"}></video>
                     </div>
                     <div className={"playBtn"}>
-                        <img className={"playStopBtn"} id={'playStopBtn'} src={"http://image.kuwo.cn/mpage/html5/2015/tuijian/hsmvbtnstop.png"}/>
+                        <img className={"playStopBtn"} id={'playStopBtn'}  src={"http://image.kuwo.cn/mpage/html5/2015/tuijian/hsmvbtnstop.png"}/>
                         {/*<img className={"playStartBtn"} src={"http://image.kuwo.cn/mpage/html5/2015/tuijian/hsmvbtn.png"}/>*/}
                         <img className={"playDownloadBtn"} src={"http://image.kuwo.cn/mpage/html5/2015/tuijian/mvdownbtn.png"}/>
                     </div>
