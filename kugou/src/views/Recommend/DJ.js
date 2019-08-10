d854a30bced996b5324a95e3f403ccd3d190cabd
sunli <sl17325309332@163.com>
2019-08-10 22:18:36 +0800

    ii

--- kugou/src/views/Recommend/DJ.js
+++ kugou/src/views/Recommend/DJ.js
@@ -35,9 +35,7 @@ import {
 				</div>
 					<div>
 						{
-	
 							this.state.child.map((v,i)=>{
-								
 								return(
 								<Link  key={i} to={"/XdJ/"+v.sourceid}>
 									<div key={i} className="tList">
